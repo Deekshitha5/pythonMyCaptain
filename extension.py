04b760797e8566191c66e4a910986d690b6f3269
Deekshitha5 <89570581+Deekshitha5@users.noreply.github.com>
2023-02-15 19:50:36 +0530

    created extension.py
    Displays the extensions of few specific files.

---
+++ extension.py
@@ -0,0 +1,9 @@
+filename = input("Input the filename:")
+if ".py" in filename:
+    print("The extension of file :'Python'")
+elif ".java" in filename:
+    print("The extension of file :'Java'")
+elif ".c" in filename:
+    print("The extension of file :'C'")
+else:
+        print("file extension not found")
